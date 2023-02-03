917c7a4037c0f17c62adbc5d1fadb07e376cfff7
zhangpeihao <zhangpeihao@kingsoft.com>
2023-02-03 10:46:01 +0800

    Daily algorithm practice submission

---
+++ leetcode/editor/cn/[offer_26]isSubStructure.ts
@@ -0,0 +1,64 @@
+//输入两棵二叉树A和B，判断B是不是A的子结构。(约定空树不是任意一个树的子结构) 
+//
+// B是A的子结构， 即 A中有出现和B相同的结构和节点值。 
+//
+// 例如: 给定的树 A: 
+//
+// 3 / \ 4 5 / \ 1 2 给定的树 B： 
+//
+// 4 / 1 返回 true，因为 B 与 A 的一个子树拥有相同的结构和节点值。 
+//
+// 示例 1： 
+//
+// 输入：A = [1,2,3], B = [3,1]
+//输出：false
+// 
+//
+// 示例 2： 
+//
+// 输入：A = [3,4,5,1,2], B = [4,1]
+//输出：true 
+//
+// 限制： 
+//
+// 0 <= 节点个数 <= 10000 
+//
+// Related Topics 树 深度优先搜索 二叉树 👍 685 👎 0
+
+
+class TreeNode {
+    val: number
+    left: TreeNode | null
+    right: TreeNode | null
+    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+        this.val = (val===undefined ? 0 : val)
+        this.left = (left===undefined ? null : left)
+        this.right = (right===undefined ? null : right)
+    }
+}
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function isSubStructure(A: TreeNode | null, B: TreeNode | null): boolean {
+    if(!A||!B) return false;
+    const dfs=(A:TreeNode,B:TreeNode)=>{
+        if(!B) return true;
+        if(!A) return false;
+        return A.val===B.val&&dfs(A.left,B.left)&&dfs(A.right,B.right);
+    };
+
+    return dfs(A,B)||isSubStructure(A.left,B)||isSubStructure(A.right,B)
+};
+//leetcode submit region end(Prohibit modification and deletion)
